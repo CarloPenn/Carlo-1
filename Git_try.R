164cef2c795dc629459adaed9125f2193976d390
Carlo Penn <Carlo.Penn@campus.lmu.de>
2022-04-25 08:56:22 +0200

    Update Git_try.R

--- Git_try.R
+++ Git_try.R
@@ -8,3 +8,7 @@ gh_token_help()
 create_github_token()
 GITHUB_PAT = "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"
 # something really special happened here
+ x <- matrix(nrow = 4, ncol = 2, byrow = TRUE, data = c(1, 2, 3, 4, 5, 6, 7, 8))
+ x
+ 
+ 
