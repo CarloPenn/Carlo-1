437a7f255eed2f6c50a37856ea1c52baaf0e6e3e
Carlo Penn <Carlo.Penn@campus.lmu.de>
2022-04-25 08:49:47 +0200

    Create Git_try.R

---
+++ Git_try.R
@@ -0,0 +1,9 @@
+x <- c(1,2,3,4)
+mean(x)
+#install.packages("usethis")
+library(usethis)
+?use_github
+
+gh_token_help()
+create_github_token()
+GITHUB_PAT = "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"
