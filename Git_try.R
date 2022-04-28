eb256eb634cb12d146a49cebf36ff569c68ede60
Carlo Penn <Carlo.Penn@campus.lmu.de>
2022-04-28 17:23:03 +0200

    Update Git_try.R

--- Git_try.R
+++ Git_try.R
@@ -1,19 +1,25 @@
-x <- c(1,2,3,4)
+x <- c(1, 2, 3, 4)
 mean(x)
 #install.packages("usethis")
 library(usethis)
-?use_github
+? use_github
 
 gh_token_help()
 create_github_token()
 GITHUB_PAT = "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"
 # something really special happened here
-x <- matrix(nrow = 4, ncol = 2, byrow = TRUE, data = c(1, 2, 3, 4, 5, 6, 7, 8))
+x <-
+  matrix(
+    nrow = 4,
+    ncol = 2,
+    byrow = TRUE,
+    data = c(1, 2, 3, 4, 5, 6, 7, 8)
+  )
 x
- 
-v1 <- c(1, 2, 3)
- v2 <- c("a", "b", "c")
- df <- data.frame("v1" = v1, "v2" = v2)
- print(df)
 
- 
+v1 <- c(1, 2, 3)
+v2 <- c("a", "b", "c")
+df <- data.frame("v1" = v1, "v2" = v2)
+print(df)
+# Cooler code
+
