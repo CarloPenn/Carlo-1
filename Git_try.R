2c80dcf018de516ac08cd3151b5dbc7506a72aa2
Carlo Penn <Carlo.Penn@campus.lmu.de>
2022-04-28 15:06:54 +0200

    Update Git_try.R

--- Git_try.R
+++ Git_try.R
@@ -8,7 +8,12 @@ gh_token_help()
 create_github_token()
 GITHUB_PAT = "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"
 # something really special happened here
- x <- matrix(nrow = 4, ncol = 2, byrow = TRUE, data = c(1, 2, 3, 4, 5, 6, 7, 8))
- x
+x <- matrix(nrow = 4, ncol = 2, byrow = TRUE, data = c(1, 2, 3, 4, 5, 6, 7, 8))
+x
  
+v1 <- c(1, 2, 3)
+ v2 <- c("a", "b", "c")
+ df <- data.frame("v1" = v1, "v2" = v2)
+ print(df)
+
  
